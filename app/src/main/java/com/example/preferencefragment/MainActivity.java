9ab77dbae172862bae4b214a861bcd8de2adbdaa
grapeyu <3392736094@qq.com>
2019-05-05 12:59:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/preferencefragment/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.preferencefragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content,new PreFragment()).commit();
+    }
+}
---
+++ app/src/main/java/com/example/preferencefragment/PreFragment.java
@@ -0,0 +1,12 @@
+package com.example.preferencefragment;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class PreFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
